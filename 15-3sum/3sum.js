1b980f3147ec28b2aacdefd9ba39360f8eac9c90
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2023-11-15 17:32:42 +0530

    Time: 138 ms (93.00%) | Memory: 59.3 MB (40.01%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,35 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let resultArr = [];
-    const sortedArr = nums.sort((a, b) => a - b);
-    for (i = 0; i < sortedArr.length - 2; i++) {
-      if (sortedArr[i] > 0) {
-        break;
-      }
-      if (i > 0 && sortedArr[i] === sortedArr[i - 1]) {
-        continue;
-      }
-      let left = i + 1;
-      let right = sortedArr.length - 1;
-      while (left < right) {
-        const sum = sortedArr[i] + sortedArr[left] + sortedArr[right];
-        if (sum < 0) {
-          left++;
-        } else if (sum > 0) {
-          right--;
-        } else {
-          resultArr.push([sortedArr[i], sortedArr[left], sortedArr[right]]);
-          while(left < right && sortedArr[left] === sortedArr[left + 1]) {
-            left++;
-          }
-          while(left < right && sortedArr[right] === sortedArr[right - 1]) {
-            right--;
-          }
-          left++;
-          right--;  
+    const res = [];
+    nums.sort((a,b) => a-b)
+
+    for (let i = 0; i < nums.length; i++) {
+        const a = nums[i];
+        if (a > 0) break;
+        if (i > 0 && a === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+        while (l < r) {
+            const threeSum = a + nums[l] + nums[r];
+            if (threeSum > 0) {
+                r--;
+            } else if (threeSum < 0) {
+                l++;
+            } else {
+                res.push([a, nums[l], nums[r]]);
+                l++;
+                r--;
+                while (nums[l] === nums[l - 1] && l < r) {
+                    l++;
+                }
+            }
         }
-      }
     }
-    return resultArr;
-};
+    return res;
+}
