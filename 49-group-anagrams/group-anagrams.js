13abdd6052a3b53e8a01864154ae0537c8061c5f
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2023-11-23 21:40:43 +0530

    Time: 122 ms (30.07%) | Memory: 54.9 MB (24.93%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = (words, map = new Map()) => {
+    if (!words.length) return [];
+
+    groupWords(words, map);    /* Time O(N * K) | Space O(N * K) */
+
+    return [ ...map.values() ];/* Time O(N)     | Space O(N * K) */
+}
+
+var groupWords = (words, map) => {
+    for (const original of words) {/* Time O(N) */
+        const hash = getHash(original); /* Time O(K) | Space O(1) */
+        const values = map.get(hash) || [];
+
+        values.push(original);          /*           | Space O(N) */
+        map.set(hash, values);          /*           | Space O(N * K) */
+    }
+}
+
+const getHash = (word) => {
+    const frequency = new Array(26).fill(0);
+
+    for (const char of word) {/* Time O(K) */
+        const charCode = getCode(char);/* Time O(1) | Space (1) */
+
+        frequency[charCode]++;         /*           | Space O(1) */
+    }
+
+    return buildHash(frequency)
+}
+
+const getCode = (char) => char.charCodeAt(0) - 'a'.charCodeAt(0);
+
+const buildHash = (frequency) => frequency.toString();
+
+
+
+
+
