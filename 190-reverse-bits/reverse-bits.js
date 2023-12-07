59860250f5c60a7a726dbc020ea561007130457f
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2023-12-07 21:55:25 +0530

    Time: 66 ms (17.47%) | Memory: 44 MB (59.69%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n, bit = 0) {
+    for (let i = 0; i < 32; i++) {
+        bit <<= 1;      // Double * 2
+        bit |= (n & 1); // Flip
+        n >>=  1;           // Reduce * 0.5
+    }
+
+    return bit >>> 0;
+};
