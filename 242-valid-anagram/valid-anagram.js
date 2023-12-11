cc0ba5d6c9c8e04e8dee1a08aca1ca0bbd956b82
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2023-12-11 20:45:28 +0530

    Time: 64 ms (79.36%) | Memory: 45.1 MB (43.93%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = (s, t, map = new Map()) => {
+    const isEqual = s.length === t.length;
+    if (!isEqual) return false;
+
+    addFrequency(s, map);      /* Time O(N) | Space O(1) */
+    subtractFrequency(t, map); /* Time O(N) | Space O(1) */
+
+    return checkFrequency(map);/* Time O(N) */
+};
+
+const addFrequency = (str, map) => {
+    for (const char of str) {/* Time O(N) */
+        const count = (map.get(char) || 0) + 1;
+
+        map.set(char, count);   /* Space O(1) */
+    }
+}
+
+const subtractFrequency = (str, map) => {
+    for (const char of str) {/* Time O(N) */
+        if (!map.has(char)) continue;
+
+        const count = map.get(char) - 1;
+
+        map.set(char, count);   /* Space O(1) */
+    }
+};
+
+const checkFrequency = (map) => {
+    for (const [ char, count ] of map) {/* Time O(N) */
+        const isEmpty = count === 0;
+        if (!isEmpty) return false;
+    }
+
+    return true;
+}
+
+
+
